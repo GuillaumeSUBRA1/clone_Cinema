11276da5d787e0b8f3fa76b3242b75077d9c0287
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2024-03-26 21:53:58 +0100

    init app module

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,22 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AccueilComponent } from './accueil/accueil.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import {MatTabsModule} from '@angular/material/tabs';
+import { FilmsComponent } from './material/films/films.component';
+import {MatListModule} from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
+import { AccueilComponent } from './material/accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccueilComponent
+    AccueilComponent,
+    FilmsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    HttpClientModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatTableModule
   ],
   providers: [],
-  bootstrap: [AppComponent,AccueilComponent]
+  bootstrap: [AppComponent]
 })
+
 export class AppModule { }
