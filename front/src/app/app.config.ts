ee4f8383b57e6f161e4a0974117f4f1b9c0db221
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2024-11-21 22:53:53 +0100

    add http

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -2,10 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from "@angular/platform-browser/animations";
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors, withXsrfConfiguration } from '@angular/common/http';
+import { authInterceptor } from './services/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideHttpClient(),
     provideAnimations()
   ]
 };
---
+++ front/src/app/services/auth.interceptor.ts
@@ -0,0 +1,21 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { tap } from 'rxjs';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+//  const authService = inject(AuthService);
+  return next(req).pipe(
+    tap({
+      error: (e: HttpErrorResponse) => {
+        /*if (e.status === 401 && e.url && e.url.includes('auth/get-authenticated-user') && authService.isAuthenticated()) {
+          authService.login();
+        } else if (e.url && 
+          ((req.method !== 'GET' && !e.url.endsWith('song/add')) || 
+          (e.url && !e.url.endsWith('auth/get-authenticated-user')) && 
+          !authService.isAuthenticated())) {
+          authService.openOrCloseAuthPopup(AuthPopupStateEnum.OPEN);
+        }*/
+      }
+    })
+  );
+};
