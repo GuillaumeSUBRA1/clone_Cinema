141c0be47d2ca41505b17eb551c45e8d88a5f69a
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2024-11-21 23:39:18 +0100

    add SecurityConfig

---
+++ back/src/main/java/com/cinema/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.cinema.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName(null);
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults());
+//                .oauth2Login(Customizer.withDefaults())
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+//                .oauth2Client(Customizer.withDefaults());
+        return http.build();
+    }
+}
