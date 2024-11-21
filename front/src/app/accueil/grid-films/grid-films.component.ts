465667ecdb2700815f6b47f11ecedfbaf0de92e3
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2024-11-21 23:48:21 +0100

    add grid-films

---
+++ front/src/app/accueil/grid-films/grid-films.component.ts
@@ -0,0 +1,50 @@
+import { Component, effect, inject, OnInit } from '@angular/core';
+import { DisplayMovieCard, IMovie } from 'src/app/model/movie.model';
+import { Pagination } from 'src/app/model/request.model';
+import { StatusNotificationEnum } from 'src/app/model/state.model';
+import { MovieService } from 'src/app/services/movie-service.service';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+
+@Component({
+  standalone: true,
+  imports: [ MovieCardComponent],
+  selector: 'grid-films',
+  templateUrl: './grid-films.component.html',
+  styleUrl: './grid-films.component.scss'
+})
+export class GridFilmsComponent implements OnInit {
+
+  movieService = inject(MovieService);
+
+  movies?: Array<DisplayMovieCard>;
+  movie?: IMovie;
+  pageRequest: Pagination = { size: 20, page: 0, sort: [] };
+
+  constructor() {
+    this.listenGetMovies();
+    this.listenGetMovie();
+  }
+
+  ngOnInit(): void {
+    this.movieService.findAll(this.pageRequest);
+  }
+
+  listenGetMovies() {
+    effect(() => {
+      const state = this.movieService.getAllMoviesSignal();
+      if (state.status === StatusNotificationEnum.OK) {
+        this.movies = state.value?.content;
+      }
+    });
+  }
+
+  listenGetMovie() {
+    effect(() => {
+      const state = this.movieService.getMovie();
+      if (state.status === StatusNotificationEnum.OK) {
+        this.movie = state.value!;
+      }
+    });
+  }
+
+}
