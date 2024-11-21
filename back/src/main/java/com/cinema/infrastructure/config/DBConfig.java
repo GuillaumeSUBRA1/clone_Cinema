b5948f7eae4b0d5729712997ea36e513acf1154f
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2024-11-21 23:44:57 +0100

    add DBConfig

---
+++ back/src/main/java/com/cinema/infrastructure/config/DBConfig.java
@@ -0,0 +1,13 @@
+package com.cinema.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories({"com.cinema.repository"})
+@EnableTransactionManagement
+@EnableJpaAuditing
+public class DBConfig {
+}
