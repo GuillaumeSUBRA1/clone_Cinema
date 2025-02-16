08918ae8e60671fc3b8f75d2592bf0c54e80bf8e
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2025-02-16 21:59:33 +0100

    remove movieTitle + add menu to cancel

--- front/src/app/accueil/reservation/reservation.component.ts
+++ front/src/app/accueil/reservation/reservation.component.ts
@@ -1,24 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component, effect, inject, OnInit } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import { ButtonModule } from 'primeng/button';
 import { SearchBook } from 'src/app/model/booking.model';
 import { ReservationSearchForm } from 'src/app/model/reservation.model';
 import { StatusNotificationEnum } from 'src/app/model/state.model';
 import { BookingService } from 'src/app/services/booking-service.service';
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+import { MenuItem } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'reservation',
   standalone: true,
-  imports: [ButtonModule, CommonModule, ReactiveFormsModule],
+  imports: [ButtonModule, MenuModule, CommonModule, ReactiveFormsModule],
   templateUrl: './reservation.component.html',
   styleUrl: './reservation.component.scss'
 })
 export class ReservationComponent {
   formBuilder = inject(FormBuilder);
   bookService = inject(BookingService);
+  router = inject(Router);
 
   books: Array<SearchBook> = [];
+  menuItems: MenuItem[] | undefined;
 
   constructor() {
     this.listenSearch();
@@ -42,6 +47,20 @@ export class ReservationComponent {
     if (this.emailForm.get('email')!.value && this.emailForm.get('email')!.value!.length > 5) {
       this.bookService.getSearchBook(this.emailForm.get('email')!.value!);
     }
+  }
+
+  showMenu(e: Event, book: SearchBook, menu: any) {
+    this.menuItems = [
+      {
+        items: [
+          {
+            label: 'Annuler',
+            command: () => this.cancel(book.id)
+          }
+        ]
+      }
+    ];
+    menu.toggle(e);
   }
 
   cancel(id: number) {
