eaa11757e101e0c2b414c528ec4b1b01b3b5f20f
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2025-02-16 22:43:43 +0100

    fix menus

--- front/src/app/model/banniere.model.ts
+++ front/src/app/model/banniere.model.ts
@@ -5,7 +5,7 @@ export interface IMenu{
 
 export const menuList: IMenu[] = [
   { name: 'Accueil', route: '/' }, 
-  { name: 'Horaires', route: 'horaires' }, 
-  { name: 'Réservation', route: 'reservation' },
-  { name: 'Infos', route: 'infos' }
+  { name: 'Horaires', route: 'horaire' }, 
+  { name: 'Réservations', route: 'reservation' },
+  { name: 'Infos', route: 'info' }
 ];
